ef09fa5d8583a289096031e326508f8e9b6caac9
blueback <bluebackpandey@gmail.com>
2024-11-14 12:55:11 +0530

    install tmux prerequisites

---
+++ scripts/tmux/tmux_install_prerequisites.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Modeline {
+#	 vi: foldmarker={,} foldmethod=marker foldlevel=0 tabstop=4 filetype=sh
+# }
+
+#
+sudo apt install libevent-dev ncurses-dev build-essential bison pkg-config
