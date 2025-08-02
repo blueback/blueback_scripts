c4073cde77aa7694b9bd1a962ba1acc69401ed81
blueback <bluebackpandey@gmail.com>
2025-08-02 17:59:37 +0530

    adding support for arch linux commands

--- scripts/encr_partition/README.md
+++ scripts/encr_partition/README.md
@@ -1,19 +1,20 @@
-
-
-
 command to open encrypted partition
 ===================================
 1. decrypt
+
     sudo cryptsetup luksOpen /dev/sda1 my_encrypted_partition
 
 will be available in /dev/mapper/my_encrypted_partition
 2. mount
+
     sudo mount /dev/mapper/my_encrypted_partition /mnt/my_mount_point
 
 3. unmount
+
     sudo umount /mnt/my_mount_point
 
 4. encrypt
+
     sudo cryptsetup luksClose my_encrypted_partition
 
 
--- scripts/misc/install_misc_apps.sh
+++ scripts/misc/install_misc_apps.sh
@@ -3,12 +3,43 @@
 #	 vi: foldmarker={,} foldmethod=marker foldlevel=0 tabstop=4 filetype=sh
 # }
 
-#
-sudo apt install tree
-sudo apt install make
-sudo apt install cmake
-sudo apt install htop
-sudo apt install ripgrep
-sudo apt install numactl
-sudo apt install python3
-sudo apt install python3-pip
+
+# check apt vs pacman {
+IS_APT=1
+if command -v apt &> /dev/null; then
+    echo "apt is installed (Debian/Ubuntu-based system)."
+    # Add apt-specific commands here
+    # Example: sudo apt update
+	IS_APT=1
+elif command -v pacman &> /dev/null; then
+    echo "pacman is installed (Arch Linux-based system)."
+    # Add pacman-specific commands here
+    # Example: sudo pacman -Syu
+	IS_APT=0
+else
+    echo "Neither apt nor pacman found. Unknown package manager."
+	IS_APT=0
+fi
+# }
+
+# {
+if [ ${IS_APT} -eq 1 ]; then
+    sudo apt install tree
+    sudo apt install make
+    sudo apt install cmake
+    sudo apt install htop
+    sudo apt install ripgrep
+    sudo apt install numactl
+    sudo apt install python3
+    sudo apt install python3-pip
+else
+    sudo pacman -S tree
+    sudo pacman -S make
+    sudo pacman -S cmake
+    sudo pacman -S htop
+    sudo pacman -S ripgrep
+    sudo pacman -S numactl
+    sudo pacman -S python3
+    sudo pacman -S python3-pip
+fi
+# }
